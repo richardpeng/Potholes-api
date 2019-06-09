d22608aca9eb4aa5c3cbfd3705549cf0543ca1cc
Richard Peng <richard@richardpeng.com>
2019-06-08 21:00:18 -0400

    Add elasticsearch

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,6 +1,19 @@
+require 'elasticsearch'
 class DataController < ApplicationController
   def raw
-    puts request.body.read
+    data = JSON.parse request.body.read
+    bulk = data.map do |d|
+      {
+        index: {
+          _index: 'raw',
+          _type: '_doc',
+          data: d
+        }
+      }
+    end
+    client = Elasticsearch::Client.new log: true, url: ENV['ELASTICSEARCH_URL']
+    client.bulk body: bulk
+
     render :nothing
   end
 end
