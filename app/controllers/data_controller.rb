97f0d57426abe977a1990edfe233db321e9fd69a
Richard Peng <richard@richardpeng.com>
2019-06-09 11:30:22 -0400

    Add map view with heatmap

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -16,4 +16,42 @@ class DataController < ApplicationController
 
     render :nothing
   end
+
+  def map
+    client = Elasticsearch::Client.new log: true, url: ENV['ELASTICSEARCH_URL']
+    data = client.search index: 'raw', body: {
+      size: 10000,
+      query: {
+        bool: {
+          must: [
+            {
+              range: {
+                magnitude: {
+                  gte: 2
+                }
+              }
+            },
+            {
+              exists: {
+                field: 'latitude'
+              }
+            },
+            {
+              exists: {
+                field: 'longitude'
+              }
+            }
+          ]
+        }
+      }
+    }
+    @heatmap = data['hits']['hits'].map do |d|
+      s = d['_source']
+      {
+        lat: s['latitude'],
+        lng: s['longitude'],
+        magnitude: s['magnitude']
+      }
+    end
+  end
 end
